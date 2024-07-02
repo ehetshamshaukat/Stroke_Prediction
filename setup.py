8b29e78ac4817ddd6f2d5754229fd9c69b4d45c0
ehetshamshaukat <ehetsham.s@gmail.com>
2024-07-02 05:36:05 +0500

    project structure N setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+from typing import  List
+
+hypen_e="-e ."
+def get_packages(file_path)-> List[str]:
+    requirements=[]
+    with open(file_path) as file:
+        requirements=file.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+        if hypen_e in requirements:
+            requirements.remove(hypen_e)
+
+
+    return requirements
+
+
+setup(
+    name="Stroke_Prediction",
+    version="0.1",
+    author="ehetsham",
+    author_email="ehetsham.s@gmail.com",
+    packages=find_packages()
+    install_requires=get_packages("requirements.txt")
+)
